cff5a9185878b593a6ddc21f73c218bbc484b0b1
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-07-11 17:05:18 -0300

    Expansão do vetor de indivíduos para o novo índice de bias

--- tests/evalutation_test.py
+++ tests/evalutation_test.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.evaluation import evaluate_individual
 
 # Cria genes aleatórios para testar
-genes = np.random.uniform(-1, 1, 8)
+genes = np.random.uniform(-1, 1, 9)
 
 print("Testando avaliação de indivíduo...")
 fitness = evaluate_individual(genes, n_episodes=3, render=True)
